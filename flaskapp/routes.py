bbf3b89d6abb64bc76820384978306f67282ee3c
Jakob Nacanaynay <jnac8080@gmail.com>
2024-02-09 18:53:37 -0800

    Add more routes and import flask-wtf

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,5 +1,5 @@
 from flaskapp import app
-from flask import render_template
+from flask import render_template, jsonify
 
 @app.route('/hello')
 def hello_world():
@@ -8,3 +8,15 @@ def hello_world():
 @app.route('/')
 def leaderboard():
     return render_template('leaderboard.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/add-game', methods=['POST'])
+def add_game():
+    return jsonify({'response': 'Game added successfully'})
